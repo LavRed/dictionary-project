34b0072298453f5e0cd782148e117fb160d1209e
LavRed <74660422+LavRed@users.noreply.github.com>
2022-01-25 21:09:37 -0500

    added the search engine portion to the dictionary app with an alert

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        React Dictionary App
-      </header>
+      <div className='container'>
+        <header className="App-header">
+          React Dictionary App
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
